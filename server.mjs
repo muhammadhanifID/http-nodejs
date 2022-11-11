7942e7f01569d89e3e388ea661c1aa03ebf325b0
Muhammad Hanif <mhd.hanif.id@gmail.com>
2022-11-11 15:20:59 +0700

    SA

--- server.mjs
+++ server.mjs
@@ -1,6 +1,30 @@
 import { createServer } from 'http';
 
+ const fetch = require('node-fetch');
 createServer((req, res) => {
-  res.write('Hello World!');
+  let formData = new FormData();
+	formData.append(
+		"key",
+		"ZFtrHZgY4lBAxPacQPiapdAFCCs6GQu3Vk8vnnzhrVSGqHm70msQG30UMxbgsx8B"
+	);
+	formData.append("sign", "2c1a72e53c246770b3f2aae2c80e3ff9");
+ 
+// note: use npm install node-fetch@2.0 to be able to use "require"
+
+console.log("trying ...")
+
+let body = {
+
+fetch('https://blahblah.com/blah', {
+    method: 'POST',
+    body: formdata,
+   
+})
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch (err => console.log(err))
+
+
+  res.write('Hello Worldsa!');
   res.end();
 }).listen(process.env.PORT);

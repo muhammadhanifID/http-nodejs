fbc84274cfe6d6d2454201e683c36c5eaccd1f65
Muhammad Hanif <mhd.hanif.id@gmail.com>
2022-11-11 16:28:29 +0700

    sa

--- server.mjs
+++ server.mjs
@@ -1,26 +1,27 @@
-import { createServer } from 'http';
-import fetch from 'node-fetch';
+import { createServer } from "http";
+import fetch from "node-fetch";
+import FormData from "form-data";
 
-
-createServer((req, res) => {
-  let formData = new FormData();
+createServer(async (req, res) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	let formData = new FormData();
 	formData.append(
 		"key",
 		"ZFtrHZgY4lBAxPacQPiapdAFCCs6GQu3Vk8vnnzhrVSGqHm70msQG30UMxbgsx8B"
 	);
 	formData.append("sign", "2c1a72e53c246770b3f2aae2c80e3ff9");
- 
-// note: use npm install node-fetch@2.0 to be able to use "require"
 
+	// note: use npm install node-fetch@2.0 to be able to use "require"
 
-const response = await fetch('https://reetenstore.id/api/profile', {
-	method: 'post',
-	body: formData,
-	
-});
-const data = await response.json();
+	const response = await fetch("https://reetenstore.id/api/profile", {
+		method: "post",
+		body: formData,
+	});
+	const data = await response.json();
 
-
-  res.write('Hellso Worlds');
-  res.end();
+	console.log(data);
+	res.write(JSON.stringify(data));
+	res.end();
 }).listen(process.env.PORT);
